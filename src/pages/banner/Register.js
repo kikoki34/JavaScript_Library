3949ab1e352021e538994a20a10ebdabb973e224
kikoki34 <kikoki@tutamail.com>
2024-06-12 17:42:12 +0300

    Update schema

---
+++ src/pages/banner/Register.js
@@ -0,0 +1,13 @@
+import React from "react";
+import BackTitleHeader from "../../components/BackTitleHeader";
+import styles from "../../styles/banner/layout.module.css";
+
+const Register = () => {
+  return (
+    <div className={styles.frame}>
+      <BackTitleHeader title="돌보미등록" />
+    </div>
+  );
+};
+
+export default Register;
